2ead39158867b15101c8fb9ba3f76922cc956f49
JianBBB <sbganne@naver.com>
2024-08-10 01:22:14 +0900

    Header에 ServerPort 추가를 위한 ServerPortFilter 생성 * 패키지명 규칙과 포트 규칙 준수 - 모든 API 의 Response Header 에 Server-Port Key 로 현재 실행중인 서버의 포트를 추가

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/filter/ServerPortFilter.java
@@ -0,0 +1,23 @@
+package com.sparta.msa_exam.product.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ServerPortFilter implements Filter {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Server-Port", serverPort);
+
+        filterChain.doFilter(request, response);
+    }
+}
